c81f268c6be2af6e3219e80ffd631f87345417b5
Israel <igarashiisrael@icloud.com>
2022-06-03 11:33:16 -0300

    Consumo de API - Login e Cadastro

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User;
+  userLogin: UserLogin = new UserLogin;
+  confirmarSenha: string;
+  tipoUsuario: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0);
   }
 
+  confirmaSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+    //criando metodo para confirmar senha e quando colocar no formulario vai comparar senha
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value;
+    //aqui guarda o tipo de usuario
+  }
+
+  cadastrar() {
+    this.user.type = this.tipoUsuario;
+
+
+    //operador ternario
+    // ex:
+    // condição ? verdade : falso
+    this.user.senha != this.confirmarSenha ? alert('As senhas estão INCORRETAS!') : this.authService
+      .cadastrar(this.user)
+      .subscribe((resp: User) => {
+        this.user = resp;
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+
+
+  }
 }
