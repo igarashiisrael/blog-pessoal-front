372db233171ad7e99a78c5f21f6b8edfee69b0ef
Israel <igarashiisrael@icloud.com>
2022-06-07 22:34:02 -0300

    Implementado put-delete no tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,22 @@ export class TemaService {
     return this.http.get<Tema[]>('https://igarashiisraelblog.herokuapp.com/temas', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://igarashiisraelblog.herokuapp.com/temas/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://igarashiisraelblog.herokuapp.com/temas', tema, this.token)
-
   }
+
+  putTema(tema: Tema): Observable<Tema> {
+    return this.http.put<Tema>('https://igarashiisraelblog.herokuapp.com/temas', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete<Tema>(`https://igarashiisraelblog.herokuapp.com/temas/${id}`, this.token)
+  }
+
+
+
 }
